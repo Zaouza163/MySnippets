d654857ac3f0086624dd68fe72716cd05af5b3ef
Nikita <gonrun@mail.ru>
2024-05-28 15:00:53 +0300

    qwerty

--- фильтр дублей.js
+++
@@ -1,9 +0,0 @@
-function filterDouble(array) {
-  var ids = array.map(function(item) {
-    return item.ItemId;
-  });
-
-  return array.filter(function (item, index) {
-    return ids.indexOf(item.ItemId) === index;
-  });
-}
